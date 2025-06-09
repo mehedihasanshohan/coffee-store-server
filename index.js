02fc8b5943226350b698bfc3613e6b1c00d54758
mehedi hasan <mhshohan04@gmail.com>
2025-06-09 15:53:42 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -2,11 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// mhshohan01
+// 7fEb7NtWJBjjgOJn
+
+// mongodb connecting functionality starts here
+const uri = "mongodb+srv://mhshohan01:7fEb7NtWJBjjgOJn@cluster0.satrpnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+// mongodb connecting functionality ends here
 
 app.get('/', (req, res) => {
   res.send('coffee making server is running')
