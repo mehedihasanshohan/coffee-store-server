0157ebcbc645ea9b8e443c9b56feed519c0c2f3d
mehedi hasan <mhshohan04@gmail.com>
2025-06-11 11:50:28 +0600

    update coffee

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
       res.send(result);
     })
 
+      // update coffee
+   app.get('/coffee/:id', async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: new ObjectId(id) }
+       const result = await coffeeCollection.findOne(query);
+       res.send(result);
+        })
+
     // receive data from client side
     app.post('/coffee', async(req, res) =>{
       const newCoffee = req.body;
